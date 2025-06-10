1d17f5e8f8f0b362eec1943a91fb052c83b4590e
fierylion <mawalladaniel2018@gmail.com>
2025-06-10 18:55:35 +0300

    changes

--- receipt_scraper_project/settings.py
+++ receipt_scraper_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-your_secret_key_here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["scraper.zawadii.app"]
 
 # Application definition
 INSTALLED_APPS = [
